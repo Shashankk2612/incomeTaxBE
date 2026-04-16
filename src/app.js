3325e51ea589eb2a69e04794aeccf5f6cae79f72
Shashank Suryavanshi <samshashank1995@gmail.com>
2026-04-16 18:04:12 +0530

    fix

--- src/app.js
+++ src/app.js
@@ -1,7 +1,14 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+const cors = require('cors');
 
 const app = express();
+
+app.use(cors({
+  origin: 'http://localhost:4200',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
 
 app.use(bodyParser.json());
 
